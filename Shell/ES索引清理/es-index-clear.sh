741cb12459c20a1e5ae6f8df4d9970c883eb4a57
thanos <73208462+vlinux@users.noreply.github.com>
2021-01-04 17:45:03 +0800

    Update es-index-clear.sh

--- Shell/ES索引清理/es-index-clear.sh
+++ Shell/ES索引清理/es-index-clear.sh
@@ -1,7 +1,15 @@
-<html>
-<head><title>404 Not Found</title></head>
-<body>
-<center><h1>404 Not Found</h1></center>
-<hr><center>nginx</center>
-</body>
-</html>
+#/bin/bash
+#指定日期(14天前)
+DATA=`date -d "2 week ago" +%Y.%m.%d`
+#当前日期
+time=`date`
+#删除14天前的日志
+curl -XDELETE http://127.0.0.1:9200/*-${DATA}
+if [ $? -eq 0 ];then
+echo $time"-->del $DATA log success.." >> /tmp/es-index-clear.log
+else
+echo $time"-->del $DATA log fail.." >> /tmp/es-index-clear.log
+fi
+################
+#crontab -e
+#10 1 * * * sh /apps/sh/es-index-clear.sh > /dev/null 2>&1
