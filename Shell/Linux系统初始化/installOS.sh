28d51a9687df074b2447a3ca318e4518f1fbc969
thanos <73208462+vlinux@users.noreply.github.com>
2021-01-20 11:32:19 +0800

    Update installOS.sh

--- Shell/Linux系统初始化/installOS.sh
+++ Shell/Linux系统初始化/installOS.sh
@@ -10,19 +10,20 @@ rm -rf /etc/yum.repos.d/*
 curl -so /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo >/dev/null
 curl -so /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo >/dev/null
 yum repolist &>/dev/null
-yum install -y tree  wget bash-completion vim java  bash-completion-extras  lrzsz net-tools sysstat iotop iftop htop unzip  telnet  ntpdate >/dev/null
+yum install -y tree  wget bash-completion vim java socat git bash-completion-extras  lrzsz net-tools sysstat iotop iftop htop unzip  telnet  ntpdate >/dev/null
 ntpdate time1.aliyun.com >/dev/null
-curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun &>>/dev/null
-systemctl start docker 
-cat >> /etc/docker/daemon.json << EOF
-{
-    "graph": "/data/docker",
-    "storage-driver": "overlay2",
-    "insecure-registries": ["registry.access.redhat.com","quay.io","harbor.kococ.cn"],
-    "registry-mirrors": ["https://q2gr04ke.mirror.aliyuncs.com"],
-    "exec-opts": ["native.cgroupdriver=systemd"],
-    "live-restore": true
-}
-EOF
-systemctl restart docker
+#安装docker
+#curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun &>>/dev/null
+#systemctl start docker 
+#cat >> /etc/docker/daemon.json << EOF
+#{
+#    "graph": "/data/docker",
+#    "storage-driver": "overlay2",
+#    "insecure-registries": ["registry.access.redhat.com","quay.io","harbor.kococ.cn"],
+#    "registry-mirrors": ["https://q2gr04ke.mirror.aliyuncs.com"],
+#    "exec-opts": ["native.cgroupdriver=systemd"],
+#    "live-restore": true
+#}
+#EOF
+#systemctl restart docker
 echo -e "${Green}初始化完成,请正常使用${Font}"
